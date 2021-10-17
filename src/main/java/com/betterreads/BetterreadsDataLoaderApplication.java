56b99bae3bc3145e69bbeba880465ece2052336d
Karthick R1 <karthick.r1@synapse.com>
2021-10-17 18:20:28 +0530

    Added bean configuration properties to connect to datastax cassandra DB.

--- src/main/java/com/betterreads/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/betterreads/BetterreadsDataLoaderApplication.java
@@ -5,16 +5,20 @@ import java.nio.file.Path;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 import connection.DataStaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 
+	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties) {
 		Path bundle = dataStaxAstraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
