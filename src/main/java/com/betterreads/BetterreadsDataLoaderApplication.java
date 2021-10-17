6b3cc94185bfefae2f13ba4669189a95bce35acb
Karthick R1 <karthick.r1@synapse.com>
2021-10-17 20:26:52 +0530

    Added authors test data and write operation to the application.

--- src/main/java/com/betterreads/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/betterreads/BetterreadsDataLoaderApplication.java
@@ -2,11 +2,17 @@ package com.betterreads;
 
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+
+import com.betterreads.author.Author;
+import com.betterreads.author.AuthorRepository;
 
 import connection.DataStaxAstraProperties;
 
@@ -14,8 +20,20 @@ import connection.DataStaxAstraProperties;
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
+	@Autowired
+	AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+	@PostConstruct
+	public void start() {
+		Author author = new Author();
+		author.setId("id");
+		author.setName("Name");
+		author.setPersonalName("personalName");
+		authorRepository.save(author);
 	}
 
 	@Bean
